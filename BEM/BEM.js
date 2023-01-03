df3c54876e140f8dc0506eb9ffcf46da8322db39
hhthuy <hohongthuy001@gmail.com>
2023-01-03 13:37:48 +0700

    html-css

---
+++ BEM/BEM.js
@@ -0,0 +1,47 @@
+/**
+BEM là gì?
+    BEM (Block Element Modifier) là tiêu chuẩn đặt tên class khi viết CSS
+# Ý nghĩa: 
+    Viết tắt: Block Element Modifier
+    Block: khối
+    Element: th ànhphần trong khối
+    Modifier: ổ sung nghĩa cho “Block và Element”
+
+## Tại sao phải dùng BEM
+    Mỗi ngườiặt một kiểu
+    Membersặt class trùng nhau, CSS đè lên nhau
+
+## Cú pháp
+.block
+
+    .block_element
+.block_element–modifier
+    .block–modifier
+
+## Tính ứng dụng
+    Xây dựng  th ànhphần trnê website
+    Xây dựng layout website
+
+## Ưu điểm, nhược điểm
+    Ưu điểm
+        Tính rõ ràng
+        Tái sử d ụngdễ dàng
+        Giúp cả team làm việc với nhau dễ dàng
+        Tính module. không lo CSS của class này  ảnh hư ởngCSS class kia
+
+    Nhược điểm
+        Tên class dài
+        Một số người cho là xấu
+
+
+## Khi nào dùng BEM là phù hợp
+        Dự  án nhiều members
+        Dự  án lớn, số lượng page nhiều or số lượng trên các th ànhần giao di ện nhiều
+        
+## Thực hành
+        Làm Button
+            - Enabled: Pointer, hover effect
+            - Disabled: Arrowm no effect
+        Làm Message
+        Làm 1 th ànhphần trên website
+*/
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # F8-HTML-CSS
-Basic HTML-CSS programming at F8
+## Basic HTML-CSS programming at F8
+***
+>[Myreport](https://docs.google.com/document/d/17Dmw9fUJfpM4qIYk1pLFmWJRaSrR5O0e/edit#)
+
+1. Xây dựng được giao diện website
+2. Phân tích giao diện website
+3. Đặt tên class theo chuẩn BEM
+4. Xây dựng layout với Flexbox
+5. Làm hiệu ứng animation
+6. Xây dựng giao diện responsive
+7. Kỹ thuật nâng cao khác
+## Mindmap
+![Mindmap](./HTML/Image/F8-Workflow.drawio.png)
+
+
+
